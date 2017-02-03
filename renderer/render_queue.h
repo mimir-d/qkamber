1055b36fa7027aa31cf5932b46fb86d9d977aafb
mimir-d <mimir-d@users.noreply.github.com>
2017-02-03 03:40:20 +0200

    add RenderQueue with world matrices and meshes
    The render queue keeps a drawing list for meshes and their world transforms.
    Will be used by the future scene system when filtering game objects.

---
+++ renderer/render_queue.h
@@ -0,0 +1,66 @@
+#pragma once
+
+// render queue has render groups
+// renderer processes queue per frame / iterator
+// render group has solids/transparent/etc as render compounds
+//
+
+class mat4;
+class Mesh;
+
+class RenderQueue
+{
+public:
+    struct Item
+    {
+        mat4& world_matrix;
+        Mesh& mesh;
+        // material
+
+        Item(mat4& world_matrix, Mesh& mesh);
+    };
+
+    using iterator = std::vector<Item>::const_iterator;
+
+public:
+    iterator begin();
+    iterator end();
+
+    template <typename... Args>
+    void add(Args&&... args);
+    void clear();
+
+private:
+    std::vector<Item> m_items;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// RenderQueue::Item impl
+///////////////////////////////////////////////////////////////////////////////
+inline RenderQueue::Item::Item(mat4& world_matrix, Mesh& mesh) :
+    world_matrix(world_matrix), mesh(mesh)
+{}
+
+///////////////////////////////////////////////////////////////////////////////
+// RenderQueue impl
+///////////////////////////////////////////////////////////////////////////////
+inline RenderQueue::iterator RenderQueue::begin()
+{
+    return m_items.cbegin();
+}
+
+inline RenderQueue::iterator RenderQueue::end()
+{
+    return m_items.cend();
+}
+
+template <typename... Args>
+inline void RenderQueue::add(Args&&... args)
+{
+    m_items.emplace_back(std::forward<Args>(args)...);
+}
+
+inline void RenderQueue::clear()
+{
+    m_items.clear();
+}
