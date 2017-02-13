c5d18276715461b2b0b1d5806daefdb2260f29f5
mimir-d <mimir@mail.bg>
2017-02-14 01:09:04 +0200

    fix camera z to point the right way
    Also fix the viewport params, since the window y is reversed.

--- renderer/camera.cpp
+++ renderer/camera.cpp
@@ -7,7 +7,7 @@
 void FpsCamera::set_proj_params(int width, int height)
 {
     const float aspect = static_cast<float>(width) / height;
-    m_proj = mat4::proj_perspective(PI_4, aspect, 0.01f, 100.0f);
+    m_proj = mat4::proj_perspective(PI_4, aspect, 0.1f, 100.0f);
 }
 
 void FpsCamera::update(float abs_time, float elapsed_time)
@@ -15,9 +15,10 @@ void FpsCamera::update(float abs_time, float elapsed_time)
     // compute camera rotation
     m_rotation += get_rotation_delta();
     m_rotation.x() = clamp(m_rotation.x(), -PI_2, PI_2);
+    m_rotation.y() = fmod(m_rotation.y(), 2 * PI);
 
     // TODO: also fix this, template rotate to produce matN
-    const mat4 r4 = mat4::rotate(m_rotation.x(), m_rotation.y(), 0.0f);
+    mat4 r4 = mat4::rotate(m_rotation.x(), m_rotation.y(), 0.0f);
     const mat3 rotation {
         r4[0][0], r4[0][1], r4[0][2],
         r4[1][0], r4[1][1], r4[1][2],
@@ -31,7 +32,7 @@ void FpsCamera::update(float abs_time, float elapsed_time)
     const vec3 position_delta = get_position_delta(elapsed_time);
     m_position += rotation * position_delta;
 
-    m_view = mat4::lookat(m_position, m_position + ahead, up);
+    m_view = mat4::lookat(m_position, m_position - ahead, up);
 }
 
 vec2 FpsCamera::get_rotation_delta()
@@ -56,7 +57,7 @@ vec3 FpsCamera::get_position_delta(float elapsed_time)
     const vec3 accel = vec3
     {
         keyboard.get_key_pressed('A') ? -1.0f : keyboard.get_key_pressed('D') ? 1.0f : 0.0f,
-        keyboard.get_key_pressed('E') ? -1.0f : keyboard.get_key_pressed('Q') ? 1.0f : 0.0f,
+        keyboard.get_key_pressed('Q') ? -1.0f : keyboard.get_key_pressed('E') ? 1.0f : 0.0f,
         keyboard.get_key_pressed('W') ? -1.0f : keyboard.get_key_pressed('S') ? 1.0f : 0.0f
     }.normalize() * m_accel_scaler;
 
