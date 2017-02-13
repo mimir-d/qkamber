013dcd9e6d508447495c72e75bab89d275456706
mimir-d <mimir@mail.bg>
2017-02-14 01:16:39 +0200

    implement frustum culling for SoftwareDevice::draw_primitive

--- renderer/software/software_device.cpp
+++ renderer/software/software_device.cpp
@@ -10,6 +10,11 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////////
 // Drawing methods
 ///////////////////////////////////////////////////////////////////////////////
+template <typename T> int sgn(T val)
+{
+    return (T(0) < val) - (val < T(0));
+}
+
 void SoftwareDevice::draw_primitive(const RenderPrimitive& primitive)
 {
     auto& mv = m_mv_matrix.get();
@@ -75,10 +80,35 @@ void SoftwareDevice::draw_primitive(const RenderPrimitive& primitive)
         vec4 v1c = mvp * vec4 { p1[0], p1[1], p1[2], 1.0f };
         vec4 v2c = mvp * vec4 { p2[0], p2[1], p2[2], 1.0f };
 
+        // infinity transition when any vertices of the triangles are on +plane and the other on -plane
+        int s0 = sgn(v0c.w());
+        int s1 = sgn(v1c.w());
+        int s2 = sgn(v2c.w());
+        if (!(s0 == s1 && s1 == s2))
+            continue;
+
         // perspective division
         v0c *= 1.0f / v0c.w();
         v1c *= 1.0f / v1c.w();
         v2c *= 1.0f / v2c.w();
+
+        // frustrum culling - left, right view planes
+        if (v0c.x() < -1.0f && v1c.x() < -1.0f && v2c.x() < -1.0f)
+            continue;
+        if (v0c.x() > 1.0f && v1c.x() > 1.0f && v2c.x() > 1.0f)
+            continue;
+
+        // frustrum culling - top, down view planes
+        if (v0c.y() < -1.0f && v1c.y() < -1.0f && v2c.y() < -1.0f)
+            continue;
+        if (v0c.y() > 1.0f && v1c.y() > 1.0f && v2c.y() > 1.0f)
+            continue;
+
+        // frustrum culling - near, far view planes
+        if (v0c.z() < 0.0f && v1c.z() < 0.0f && v2c.z() < 0.0f)
+            continue;
+        if (v0c.z() > 1.0f && v1c.z() > 1.0f && v2c.z() > 1.0f)
+            continue;
 
         // transform to device space
         vec3 v0d = m_clip_matrix * v0c;
