a3a33bce4df95c761ed6e8698a80fd61b05e3314
mimir-d <mimir-d@users.noreply.github.com>
2017-03-27 00:44:24 +0300

    add EntityConfig for file based entity configuration

--- qkamber/entity/entity_system.cpp
+++ qkamber/entity/entity_system.cpp
@@ -2,16 +2,17 @@
 #include "precompiled.h"
 #include "entity_system.h"
 
-// TODO: temp until entity factory?
-#include "engine.h"
+#include "entity_config.h"
 #include "asset/asset_system.h"
 #include "render/render_system.h"
 #include "render/model.h"
+#include "engine.h"
 
 using namespace std;
 
 EntitySystem::EntitySystem(QkEngine::Context& context) :
-    Subsystem(context)
+    Subsystem(context),
+    m_config{ new EntityConfig{ "entities.ini" } }
 {
     flog("id = %#x", this);
     log_info("Created entity system");
@@ -27,23 +28,14 @@ unique_ptr<EntitySystem::Entity> EntitySystem::create_entity(const string& name)
 {
     flog();
 
+    // allocate id and reserve components
     eid_t eid = get_next_id();
     m_store.resize(eid + 1);
     m_mask.resize(eid + 1);
 
     // TODO: small block allocator or value-type
     auto ret = unique_ptr<Entity>(new Entity{ *this, eid, private_tag{} });
-
-    // add components
-    // TODO: base on config file
-    ret->add_component<SrtComponent>();
-
-    auto& asset = m_context.get_asset();
-    auto geometry = asset.load_geometry(name);
-    // TODO: kept a memleak here
-    auto m = new Model(*geometry.get(), m_context.get_render().get_device(), asset);
-    ret->add_component<ModelComponent>().set_model(m);
-
+    m_config->config(name, *ret.get(), m_context.get_render(), m_context.get_asset());
 
     log_info("Created entity id = %d...", eid);
     return ret;
